05dd52829d18e94edc34baa7d511e41a051e6eef
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-03-23 13:59:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,59 @@
 // Copyright 2021 NNTU-CS
+#include <alg.h>
+#include <iostream>
+#include <cstdint>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int sum = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (value == arr[i] + arr[j]) {
+                sum++;
+            }
+        }
+    }
+    return sum;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int sum = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = len-1; j > i; --j) {
+            if (value == arr[i] + arr[j]) {
+                sum++;
+            }
+        }
+    }
+    return sum;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+
+int countPairs3(int* arr, int len, int value) {
+    int result = 0;
+    for (int i = 0; i < len - 1; i++) {
+    int left = i;
+    int right = len;
+    while (left < right - 1) {
+        int middle;
+        middle = (right + left) / 2;
+        if (arr[i] + arr[middle] == value) {
+            result++;
+            int j;
+            j = middle + 1;
+            while ((arr[i] + arr[j] == value) && (j < right)) {
+                result++;
+                j++;
+            }
+            j = middle - 1;
+            while ((arr[i] + arr[j] == value) && (j > left)) {
+                result++;
+                j--;
+            }
+            break;
+            } else if (arr[i] + arr[middle] > value) {
+                right = middle;
+            } else {
+                left = middle;
+            }
+        }
+    }
+    return result;
